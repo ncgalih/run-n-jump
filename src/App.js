c744784dad0447a18907b3a9da86239287a07fe4
Nc Galih <encegalih@gmail.com>
2022-02-14 18:48:41 +0700

    Run Player

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import usePlayer from './Engine/usePlayer';
 
 function App() {
   const player = usePlayer()
+  useEffect(()=>{
+    document.addEventListener('keydown',(e)=>{
+      if(e.key==='ArrowUp'||e.key===' ') if(!player.isJumping) player.jump()
+      if(e.key==='ArrowRight') {
+        if(!player.isRunning) player.run('right')
+      }
+      if(e.key==='ArrowLeft') {
+        if(!player.isRunning) player.run('left')
+      }
+    })
+    document.addEventListener('keyup', (e)=> {
+      if(e.key==='ArrowRight'||e.key==='ArrowLeft') player.stopRun()
+    })
+    return () => document.removeEventListener()
+  }, [])
   return (
     <div className="canvas">
       <div className='player' onClick={()=>player.jump()} style={{
@@ -12,6 +27,7 @@ function App() {
       }}>
         {player.y}
       </div>
+        <p>score: {player.x}</p>
     </div>
   );
 }
--- src/Engine/usePlayer.js
+++ src/Engine/usePlayer.js
@@ -4,10 +4,12 @@ const usePlayer = () => {
     const [x, setX] = useState(0)
     const [y, setY] = useState(0)
 
-    const velocity = (t) => t*4
+    const jump_velocity = (t) => t*4
     const gravity = (t) => t*t*9.8/2
+    const run_velocity = (t) => t*4
     const scale = 140
 
+    //Jump
     const jumpStart = useRef(null)
     const jumpRef = useRef()
     let isJumping = false
@@ -15,12 +17,10 @@ const usePlayer = () => {
     const jump = () => {
         jumpRef.current = requestAnimationFrame(jumping)
     }
-    const stoped = false
     const jumping = (timestamp) => {
-        if(stoped) return
         if(jumpStart.current===null) jumpStart.current = timestamp 
         const t = (timestamp - jumpStart.current)/1000
-        y_jump = Math.max((velocity(t)-gravity(t))*scale,0)
+        y_jump = Math.max((jump_velocity(t)-gravity(t))*scale,0)
         setY(y_jump)
         if(isJumping && y_jump===0){
             jumpStart.current = null
@@ -31,7 +31,38 @@ const usePlayer = () => {
         isJumping = true
         jumpRef.current = requestAnimationFrame(jumping)
     }
-    return {x,y, jump}
+
+    //Run 
+    const runRef = useRef()
+    let isRunning = false
+    let x_run = 0
+    let t_prevRun = null
+    let direction = 'right'
+    const run = (direct) => {
+        if(isRunning) return
+        console.log('start')
+        direction = direct
+        isRunning = true
+        runRef.current = requestAnimationFrame(running)
+    }
+    const running = (timestamp) => {
+        if(t_prevRun===null) t_prevRun=timestamp
+        const t = (timestamp - t_prevRun)/1000
+        x_run = run_velocity(t)*scale
+        if(direction==='right') setX(prevX=>prevX+x_run)
+        else setX(prevX=>prevX-x_run)
+        t_prevRun = timestamp
+        runRef.current = requestAnimationFrame(running)
+    }
+    const stopRun = () => {
+        cancelAnimationFrame(runRef.current)
+        console.log('stop')
+        isRunning = false
+        t_prevRun = null
+    }
+    
+
+    return {x,y, jump, run, stopRun, isRunning, isJumping}
 }
 
 export default usePlayer
